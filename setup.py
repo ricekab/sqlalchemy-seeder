d7397f53bd94907d1ccaa35cd98527e2a8abd158
kevincytang <kevincytang@hotmail.com>
2018-04-04 16:25:38 +0200

    Added some debug logs. Version bumped

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sqlalchemy-seeder',
-    version='0.3.1',
+    version='0.4.0',
     packages=find_packages(exclude=["tests"]),
     package_data={"sqlalchemyseeder": ["resources/*"]},
     url='https://github.com/ricekab/sqlalchemy-seeder',
